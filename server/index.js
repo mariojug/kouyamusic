49b4b858b1ebadb4913e91bb44d66ce2de1d7b9e
Mario Juguilon <mariojjuguilon@gmail.com>
2022-01-09 22:59:56 -0600

    Fixed routing conflicts between express and react

--- server/index.js
+++ server/index.js
@@ -1,15 +1,16 @@
 require("dotenv").config({ path: require("find-config")(".env") });
 const port = process.env.PORT || 8080;
-
+const path = require("path");
 const express = require("express");
 const api = require("./api/index");
 
 const app = express();
-app.use("/", express.static("build"));
+app.use(express.static(path.join(__dirname, "build")));
 app.use(express.urlencoded());
 app.use(express.json());
 
 app.use("/api", api);
+app.get("*", (req, res) => res.sendFile(path.resolve("build", "index.html")));
 
 app.listen(port);
 console.log("Server successfully started on PORT " + port);
